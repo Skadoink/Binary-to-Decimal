b5b2dcae9284124d8344b4dfe5b45198400e9941
Oskar <oskaege@gmail.com>
2021-05-02 10:15:56 +1200

    added valid check but has errors

--- src/binaryToDecimal.java
+++ src/binaryToDecimal.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class binaryToDecimal {
-    static void checkBinary(int binaryNumb) {
+    static void checkBinary(String binaryStr) {
         //if not valid, call getBinary again after error message
+        try {
+            int binaryNumb = Integer.parseInt(binaryStr);
+            while (binaryNumb != 0){
+                if (binaryNumb % 10 > 1) { //check if last digit is a 1 or 0
+                    throw new Exception();
+                }
+                binaryNumb = binaryNumb / 10; //remove last digit
+            }
+            System.out.println("Your binary number is valid.");
+        }
+        catch (Exception e){
+            System.out.println("Please enter a binary integer containing only 1's and 0's.");
+            getBinary();
+        }
     }
     static void getBinary() {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter the binary number: ");
-
-        int binaryNumb = myObj.nextInt(); //needs to make sure int is used
-        //could just use string and check conversion to int in checkBinary method
-        System.out.println("Your binary number is " + binaryNumb + ".");
+        System.out.print("Enter the binary number: ");
+        String binaryStr = myObj.nextLine(); 
+        System.out.println("You entered " + binaryStr + ".");
         myObj.close();
-        checkBinary(binaryNumb); //call method to check validity
+        checkBinary(binaryStr); //call method to check validity
     }
 
     public static void main(String[] args) {
